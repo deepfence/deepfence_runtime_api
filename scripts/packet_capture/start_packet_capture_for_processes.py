22079368b63e5a5851becf8cea273497c65597fa
Arnabjyoti Kalita <arnabjyoti@deepfence.io>
2021-04-29 05:47:20 +0000

    New packet capture APIs and removing the depcrecated APIs - deepfence/dev-issues#869

--- scripts/packet_capture/start_packet_capture_for_processes.py
+++ scripts/packet_capture/start_packet_capture_for_processes.py
@@ -1,10 +1,11 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import time
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def start_packet_capture(api_url, api_key):
+def start_packet_capture(api_url, api_key, process_list, pcap_mode):
     default_headers = {"Content-Type": "application/json", "Authorization": ""}
     api_response = requests.post("{0}/users/auth".format(api_url),
                                  json={"api_key": api_key}, headers=default_headers,
@@ -18,33 +19,24 @@ def start_packet_capture(api_url, api_key):
 
     enumerate_filters = {"type": ["host"], "pseudo": False}
     api_response = requests.post("{0}/enumerate".format(api_url),
-                                 json={"filters": enumerate_filters, "size": 1000},
+                                 json={"filters": enumerate_filters, "size": 5000},
                                  headers=default_headers, verify=False).json()
     nodes_list = []
     counter = 1
-    print("\nStart Packet Capture")
     api_response_nodes = api_response.get("data", {}).get("data", [])
     if not api_response_nodes:
         print("No nodes found")
         return
     for node in api_response_nodes:
-        if node.get("is_ui_vm", False):
+        if node.get("is_ui_vm", False) or node.get("pseudo", False):
             continue
-        if node["type"] == "kube_service":
-            node_name = "{0} (kubernetes service)".format(node.get("name", ""))
-        else:
-            node_name = "{0} (host)".format(node.get("host_name", ""))
-        print("{0}: {1}".format(counter, node_name))
-        nodes_list.append({"id": node["id"], "node_name": node_name, "node_type": node["type"]})
+        nodes_list.append({"id": node["id"], "node_name": node.get("host_name", "")})
         counter += 1
     print("\nEnter comma separated list of node numbers to start packet capture. Eg: 1,3,4\n")
     print("Enter \"all\" (without quotes) to start packet capture on all nodes\n")
-    print("Enter \"all hosts\" (without quotes) to start packet capture on all hosts\n")
     user_input = input("-->").split(",")
     if "all" in user_input:
         nodes_selected = nodes_list
-    elif "all hosts" in user_input:
-        nodes_selected = [n for n in nodes_list if n["node_type"] == "host"]
     else:
         nodes_selected = []
         for user_input_no in user_input:
@@ -55,33 +47,45 @@ def start_packet_capture(api_url, api_key):
     if not nodes_selected:
         print("No nodes selected. Select at least one node.")
         exit(0)
-    # print("\nEncrypted packet capture? Enter Y or N:")
-    # is_encrypted_capture = str(input("-->"))
-    is_encrypted_capture = "N"
+    print("Total hosts: {0}".format(counter))
+    print("\nStopping Packet Capture on all hosts, if not already stopped.")
     post_data = {
-        "action": "packet_capture_start",
+        "action": "packet_capture_stop",
         "node_type": "host",
-        "node_id_list": [n["id"] for n in nodes_selected],
-        "action_args": {
-            "packet_capture": {
-                "port_list": [], "interface_name": "All", "snap_length": 65535, "percent_capture": 100,
-                "is_encrypted_capture": is_encrypted_capture
-            }
-        }
+        "node_id_list": [n["id"] for n in nodes_selected]
     }
     try:
         response = requests.post("{0}/node_action".format(api_url), headers=default_headers,
                                  verify=False, json=post_data)
         print(response.text)
-        print("Packet capture will be started in selected nodes")
+        print("Packet capture will be stopped in a moment")
     except:
         print("Error in api call")
+
+    time.sleep(60)
+
+    print("\nStarting Packet Capture")
+    is_encrypted_capture = "N"
+    for node in nodes_selected:
+        post_data = {
+            "process_list": process_list, "snap_length": 65535,
+            "capture_percentage": 100, "is_encrypted_capture": is_encrypted_capture, "pcap_mode": pcap_mode
+        }
+        try:
+            response = requests.post("{0}/node/{1}/packet_capture_start".format(api_url, node["id"]),
+                                     headers=default_headers, verify=False, json=post_data)
+            print(response.text)
+        except:
+            print("Error in api call")
+    print("Packet capture started")
 
 
 if __name__ == '__main__':
     import sys
 
     if len(sys.argv) != 3:
-        print("Usage: python3 start_packet_capture.py <mgmt_console_ip_address> <api_key>")
+        print("Usage: python3 packet_capture.py <mgmt_console_ip_address> <api_key>")
         exit(1)
-    start_packet_capture("https://{0}/deepfence/v1.5".format(sys.argv[1]), sys.argv[2])
+    process_list = ['java']
+    pcap_mode = "allow"     #should either be 'allow', 'deny' or 'all'
+    start_packet_capture("https://{0}/deepfence/v1.5".format(sys.argv[1]), sys.argv[2], process_list, pcap_mode)
