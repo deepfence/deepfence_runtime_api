faeb09c634293992add221397fe5bddd2d235ebd
ramanan-ravi <ramanan@deepfence.io>
2021-05-27 21:07:17 +0530

    Fix readme hyperlink

--- scripts/packet_capture/start_packet_capture_for_processes.py
+++ scripts/packet_capture/start_packet_capture_for_processes.py
@@ -7,10 +7,11 @@ from queue import Queue
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 q = Queue(maxsize=0)
-thread_batch_size = 50
+thread_batch_size = 20
 retries = 15
 seconds_between_retries = 3
 capture_in_progress = "packet capture is already in progress"
+
 
 def start_packet_capture(api_url, api_key, process_list, pcap_mode):
     default_headers = {"Content-Type": "application/json", "Authorization": ""}
@@ -72,11 +73,11 @@ def start_packet_capture(api_url, api_key, process_list, pcap_mode):
     time.sleep(60)
     print("\nDeleting the saved packet capture config.")
     post_data = {
-        "host_name_list": ["all"]        
+        "host_name_list": ["all"]
     }
     try:
         response = requests.delete("{0}/packet_capture_config".format(api_url), headers=default_headers,
-                                    verify=False, json=post_data)
+                                   verify=False, json=post_data)
         print(response.text)
     except:
         print("Error in API to delete existing packet capture config")
@@ -84,15 +85,15 @@ def start_packet_capture(api_url, api_key, process_list, pcap_mode):
     is_encrypted_capture = "N"
     print("\nSaving selected packet capture config")
     post_data = {
-        "config_list": [{"config":{"process_list":process_list, 
-                                   "snap_length":65535, 
-                                   "capture_percentage":100, 
-                                   "is_encrypted_capture":is_encrypted_capture, 
-                                   "pcap_mode":pcap_mode}, "host_name":"all"}]        
+        "config_list": [{"config": {"process_list": process_list,
+                                    "snap_length": 65535,
+                                    "capture_percentage": 100,
+                                    "is_encrypted_capture": is_encrypted_capture,
+                                    "pcap_mode": pcap_mode}, "host_name": "all"}]
     }
     try:
         response = requests.post("{0}/packet_capture_config".format(api_url), headers=default_headers,
-                                    verify=False, json=post_data)
+                                 verify=False, json=post_data)
         print(response.text)
     except:
         print("Error in API while saving packet capture config.")
@@ -112,6 +113,7 @@ def start_packet_capture(api_url, api_key, process_list, pcap_mode):
     q.join()
     print("Packet capture started")
 
+
 def process_requests_from_queue(q, api_url, post_data, default_headers):
     while not q.empty():
         node = q.get()
@@ -119,23 +121,26 @@ def process_requests_from_queue(q, api_url, post_data, default_headers):
         q.task_done()
     return True
 
+
 def packet_capture_start_request_with_retry(api_url, node, post_data, default_headers):
     for i in range(retries):
         try:
             response = requests.post("{0}/node/{1}/packet_capture_start".format(api_url, node["id"]),
-                                        headers=default_headers, verify=False, json=post_data)
+                                     headers=default_headers, verify=False, json=post_data)
             print(response.text)
-            if(response.status_code != 200):
+            if (response.status_code != 200):
                 json_response = response.json()
-                if(json_response['success'] == False and json_response['error'].get('message',"") != capture_in_progress):
-                    if(i < retries - 1):
+                if (json_response['success'] == False and json_response['error'].get('message',
+                                                                                     "") != capture_in_progress):
+                    if (i < retries - 1):
                         print("Error in api call for node {node_name}. Retrying...".format(node_name=node["node_name"]))
                         time.sleep(seconds_between_retries)
                         continue
                     else:
-                        print("Api call for node {node_name} unsuccessful after retries".format(node_name=node["node_name"]))
+                        print("Api call for node {node_name} unsuccessful after retries".format(
+                            node_name=node["node_name"]))
         except:
-            if(i < retries - 1):
+            if (i < retries - 1):
                 print("Error in api call for node {node_name}. Retrying...".format(node_name=node["node_name"]))
                 time.sleep(seconds_between_retries)
                 continue
@@ -144,19 +149,20 @@ def packet_capture_start_request_with_retry(api_url, node, post_data, default_he
         else:
             break
 
+
 if __name__ == '__main__':
     import sys
 
     if len(sys.argv) != 5:
-        print("Usage: python3 packet_capture.py <mgmt_console_ip_address> <api_key> <comma_separated_process_list> <pcap_mode>")
+        print(
+            "Usage: python3 packet_capture.py <mgmt_console_ip_address> <api_key> <comma_separated_process_list> <pcap_mode>")
         exit(1)
     if type(sys.argv[3]) != str:
         print("The process list must be a comma separated string.")
         exit(1)
-        
+    # argv[3]: "java,fluentd"
     process_list = sys.argv[3].split(",")
-    # process_list = ['java']
-    pcap_mode = sys.argv[4] #should either be 'allow', 'deny' or 'all'
+    pcap_mode = sys.argv[4]  # should either be 'allow', 'deny' or 'all'
     if pcap_mode not in ['allow', 'deny', 'all']:
         print("pcap_mode should either be 'allow', 'deny' or 'all'")
         exit(1)
