67be27097cf797378a0c1569796b2b7135fde1f1
ramanan-ravi <ramanan@deepfence.io>
2022-01-24 18:10:07 +0530

    Update streaming api client

--- scripts/streaming_api/streaming_api.go
+++ scripts/streaming_api/streaming_api.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
-var nodeType = flag.String("node_type", "host", fmt.Sprintf("Node type to run websocket client for. Ex: host, container, container_image, container_by_name, process, process_by_name, pod, kube_service, kube_controller"))
-var apiUrl = flag.String("api_url", "", "Enter api url. Ex: 22.33.44.55")
+var nodeType = flag.String("node_type", "hosts", fmt.Sprintf("Node type to run websocket client for. Ex: hosts, containers, containers-by-image, kubernetes-clusters, cloud-providers, cloud-regions, processes"))
+var apiUrl = flag.String("api_url", "", "Enter api url. Example: 22.33.44.55 / abc.com")
 var apiKey = flag.String("api_key", "", "Enter api key. (Get it from user management page)")
-var stopped = flag.String("stopped", "running", "Filter by stopped status: stopped | running | both")
-var pseudo = flag.String("pseudo", "hide", "Filter by pseudo status: show | hide")
-var unconnected = flag.String("unconnected", "hide", "Filter by unconnected status: show | hide")
-var namespace = flag.String("namespace", "", "Filter by namespace (for kubernetes)")
+var ignoreConnections = flag.Bool("ignore_connections", true, "Weather to ignore connections data")
 
-func connectWS() *websocket.Conn {
-	wsUrl := fmt.Sprintf("wss://%s/ws?node_type=%s&stopped=%s&pseudo=%s&unconnected=%s&namespace=%s&api_key=%s", *apiUrl, *nodeType, *stopped, *pseudo, *unconnected, *namespace, *apiKey)
-	fmt.Printf("Connecting to %s", wsUrl)
+func connectWS() (*websocket.Conn, error) {
+	wsUrl := fmt.Sprintf("wss://%s/topology-api/topology-connection-ws?t=5s&ignore_connections=%s&api_key=%s",
+		*apiUrl, strconv.FormatBool(*ignoreConnections), *apiKey)
+	fmt.Printf("Connecting to %s\n", wsUrl)
 	dialer := &websocket.Dialer{
 		Proxy:            http.ProxyFromEnvironment,
 		HandshakeTimeout: 45 * time.Second,
@@ -29,20 +28,44 @@ func connectWS() *websocket.Conn {
 	}
 	conn, _, err := dialer.Dial(wsUrl, nil)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
-	return conn
+	return conn, nil
 }
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)
-	ws := connectWS()
+	ws, err := connectWS()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	sendMessage := func(msg string) error {
+		msgJson := map[string]interface{}{}
+		err := json.Unmarshal([]byte(msg), &msgJson)
+		if err != nil {
+			return err
+		}
+		err = ws.WriteJSON(msgJson)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	go func() {
+		err := sendMessage(fmt.Sprintf(`{"add":{"topology_id":"","node_id":"","children":[{"topology_id":"%s"}]}}`, *nodeType))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}()
+
 	for {
 		_, resp, err := ws.ReadMessage()
 		if err != nil {
-			fmt.Println("read:", err)
+			fmt.Println(err)
 			return
 		}
 		fmt.Println("\nGot data:\n", string(resp))
