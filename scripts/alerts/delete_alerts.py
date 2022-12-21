da5f6af465ed92303622a70cbed791fbdfd5f67f
Ramanan Ravikumar <ramanan@deepfence.io>
2022-12-21 11:25:50 +0530

    Script to bulk delete alerts
    Signed-off-by: Ramanan Ravikumar <ramanan@deepfence.io>

--- scripts/alerts/delete_alerts.py
+++ scripts/alerts/delete_alerts.py
@@ -24,35 +24,45 @@ def get_blocked_ips(api_url, api_key):
         print("Authentication failed")
         return
     default_headers["Authorization"] = "Bearer " + auth_response["data"]["access_token"]
-    page_no = 1
-    page_size = 500
-    alerts_summary = requests.post(
-        "{0}/search?from=0&size=10000&lucene_query=&number=365&time_unit=day".format(api_url, page_no, page_size),
-        json={"_type": "alert", "_source": ["_id", "classtype", "masked"], "filters": {"masked": ["false", "true"]}},
-        headers=default_headers, verify=False).json()
-    if not alerts_summary.get("success", False):
-        print(alerts_summary.get("error", ""))
-        return
+    es_from = 0
+    page_size = 5000
+    alerts = []
     classtypes = defaultdict(int)
     masked_classtypes = defaultdict(int)
-    for hit in alerts_summary["data"]["hits"]:
-        if hit["_source"]["masked"] == "true":
-            masked_classtypes[hit["_source"]["classtype"]] += 1
+    while True:
+        alerts_summary = requests.post(
+            "{0}/search?from={1}&size={2}&lucene_query=&number=365&time_unit=day".format(api_url, es_from, page_size),
+            json={"_type": "alert", "_source": ["_id", "classtype", "masked"],
+                  "filters": {"masked": ["false", "true"]}},
+            headers=default_headers, verify=False).json()
+        if not alerts_summary.get("success", False):
+            print(alerts_summary.get("error", ""))
+            return
+        if not alerts_summary["data"]["hits"]:
+            break
+        alerts.extend(alerts_summary["data"]["hits"])
+        es_from += page_size
+
+    for alert in alerts:
+        if alert["_source"]["masked"] == "true":
+            masked_classtypes[alert["_source"]["classtype"]] += 1
         else:
-            classtypes[hit["_source"]["classtype"]] += 1
+            classtypes[alert["_source"]["classtype"]] += 1
     if not classtypes and not masked_classtypes:
         print("No alerts found")
         return
 
     classtypes = [{"classtype": k, "count": v} for k, v in classtypes.items()]
-    masked_classtypes = [{"classtype": k, "count": v} for k, v in masked_classtypes.items()]
-    print("\nAlert classtype with count")
-    for i, classtype in enumerate(classtypes):
-        print("{0}. {1}\t{2}".format(i + 1, classtype["classtype"], classtype["count"]))
     classtypes_len = len(classtypes)
-    print("\nAlert classtype with count (Masked alerts)")
-    for i, classtype in enumerate(masked_classtypes):
-        print("{0}. {1}\t{2}".format(classtypes_len + i + 1, classtype["classtype"], classtype["count"]))
+    masked_classtypes = [{"classtype": k, "count": v} for k, v in masked_classtypes.items()]
+    if classtypes:
+        print("\nAlert classtype with count")
+        for i, classtype in enumerate(classtypes):
+            print("{0}. {1}\t{2}".format(i + 1, classtype["classtype"], classtype["count"]))
+    if masked_classtypes:
+        print("\nAlert classtype with count (Masked alerts)")
+        for i, classtype in enumerate(masked_classtypes):
+            print("{0}. {1}\t{2}".format(classtypes_len + i + 1, classtype["classtype"], classtype["count"]))
 
     print("\nEnter comma separated list of classtypes to delete. Eg: 1,3,4")
     user_input = input("-->").split(",")
@@ -73,13 +83,13 @@ def get_blocked_ips(api_url, api_key):
 
     alert_id = []
     for classtype in selected_classtypes:
-        for hit in alerts_summary["data"]["hits"]:
-            if hit["_source"]["masked"] != "true" and hit["_source"]["classtype"] == classtype:
-                alert_id.append(hit["_id"])
+        for alert in alerts:
+            if alert["_source"]["masked"] != "true" and alert["_source"]["classtype"] == classtype:
+                alert_id.append(alert["_id"])
     for classtype in selected_masked_classtypes:
-        for hit in alerts_summary["data"]["hits"]:
-            if hit["_source"]["masked"] == "true" and hit["_source"]["classtype"] == classtype:
-                alert_id.append(hit["_id"])
+        for alert in alerts:
+            if alert["_source"]["masked"] == "true" and alert["_source"]["classtype"] == classtype:
+                alert_id.append(alert["_id"])
     for alert_ids in split_list_into_chunks(alert_id, 500):
         print(requests.post("{0}/docs/delete_by_id".format(api_url),
                             json={"ids": alert_ids, "index_name": "alert", "doc_type": "alert"},
